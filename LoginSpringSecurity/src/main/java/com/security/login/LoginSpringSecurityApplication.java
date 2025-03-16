0dcd34a03153cd1fe8c1bdea63161cf948b772d9
rupajobs87 <106575871+rupajobs87@users.noreply.github.com>
2025-03-16 14:55:02 +0400

    Initial Commit

---
+++ LoginSpringSecurity/src/main/java/com/security/login/LoginSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringSecurityApplication.class, args);
+	}
+
+}
